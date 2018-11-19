7b56cc952818fb361330329aa87890a0fbd4af5f
keithyan2018 <40131326+keithyan2018@users.noreply.github.com>
2018-11-20 04:34:41 +0800

    added billing and client side app!

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px'}}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
+    }
+  }
+
   render() {
     return (
       <nav>
          <div className="nav-warpper">
-            <a className="left brand-logo">
+            <Link
+              to={this.props.auth ? '/surveys' : '/'}
+              className="left brand-logo"
+            >
                Emaily
-            </a>
+            </Link>
             <ul className="right">
-               <li>
-                 <a href="/auth/google">Login With Google</a>
-               </li>
+               {this.renderContent()}
             </ul>
          </div>
       </nav>
@@ -19,4 +40,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth}) {
+  return { auth };
+}
+
+export default connect(mapStateToProps) (Header);
